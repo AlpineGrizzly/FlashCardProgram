997169b5de276d7fe29b542b1b45ecc18c6a887b
Dalton Kinney <daltonckinney@gmail.com>
2022-03-28 21:12:32 -0400

    have somewhat functionality with reading from .txt files to load in study cards. Not completely working. verifies somewhat correctly, throws error when its incorrect

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ def quit():
 
 # Reads the cards into the program
 def readCard():
+    global f
     f = open('FrenchDeck.txt', 'r')
-    f.close()
+    deck = f.readline()
+    test = deck.split("`")
+    global front, back
+    #setting front and back here
+    labelFront.configure(text=front)
+    print(test)
+
 
 def closePopup(top):
     top.destroy()
@@ -44,33 +51,32 @@ def addCard():
     #exitBtn = Button(top, text="exit", command=closePopup(top))
     #exitBtn.pack()
 
-
-def getFront():
-    labelFront.configure(text="Bonjour, Dalton")
-
 def getNext():
-    labelFront.configure(text="Comment ca va?")
+    f.readline()
 
 def checkAns():
     global entryAns
+    global counter
     string = entryAns.get()
-    if(string == "Hello, Dalton"):
+    if string == back:
         labelFront.configure(text="Correct!")
-        labelFront.after(3000, getNext)
-
+        labelFront.after(1500, readCard())
+        entryAns.delete(0, END)
     else:
         labelFront.configure(text="That was not correct, Try again!")
-        labelFront.after(3000, getFront)
+        labelFront.after(1500, front)
+    counter += 1
 
-labelTitle = Label(window, height = 1,
-                   width = 500,
-                   text = "FlashStudy Program",
-                   font = ("Arial", 15))
+
+labelTitle=Label(window, height=1,
+                   width=500,
+                   text="FlashStudy Program",
+                   font=("Arial", 15))
 labelTitle.pack()
 
 labelFront = Label(window,
                    height = 10, width=40,
-                   text="Bonjour, Dalton",
+                   text="Press start to study",
                    font=("Arial", 15))
 labelFront.place(relx=0.5,
                  rely=0.5,
@@ -92,39 +98,40 @@ checkBtn.place(relx = 0.5,
                anchor = 'center')
 
 startBtn = Button(window,
-                  height = 2, width = 10,
-                  text = "Start",
-                  font = ("Arial", 10))
-startBtn.place(relx = 0,
-               rely = 0.3,
-               anchor = 'w')
+                  height=2, width=10,
+                  text="Start",
+                  font=("Arial", 10),
+                  command=readCard)
+startBtn.place(relx=0,
+               rely=0.3,
+               anchor='w')
 addcardBtn = Button(window,
-                  height = 2, width = 10,
-                  text = "Add Card",
-                  font = ("Arial", 10),
-                  command = addCard)
-addcardBtn.place(relx = 0.87,
-                 rely = 0.3,
-                 anchor = 'w')
+                  height=2, width=10,
+                  text="Add Card",
+                  font=("Arial", 10),
+                  command=addCard)
+addcardBtn.place(relx=0.87,
+                 rely=0.3,
+                 anchor='w')
 editdeckBtn = Button(window,
-                  height = 2, width = 10,
-                  text = "Edit Deck",
-                  font = ("Arial", 10))
-editdeckBtn.place(relx = 0.87,
-                  rely = 0.4,
-                  anchor = 'w')
+                  height=2, width = 10,
+                  text="Edit Deck",
+                  font=("Arial", 10))
+editdeckBtn.place(relx=0.87,
+                  rely=0.4,
+                  anchor='w')
 exitappBtn = Button(window,
-                  height = 2, width = 10,
-                  text = "Quit",
-                  font = ("Arial", 10), command=quit)
-exitappBtn.place(relx = 0,
-                  rely = 0.4,
-                  anchor = 'w')
+                  height=2, width=10,
+                  text="Quit",
+                  font=("Arial", 10), command=quit)
+exitappBtn.place(relx=0,
+                  rely=0.4,
+                  anchor='w')
 
+def main():
+    window.mainloop()
 
-
-window.mainloop()
-
-
+if __name__=="__main__":
+    main()
 
 
